67392128c1e44771754ff5a1251e225349e269c2
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-05-17 22:50:42 +0900

    第二段階のテストを更新

--- bitonic-sorter/src/lib.rs
+++ bitonic-sorter/src/lib.rs
@@ -2,3 +2,8 @@ pub mod first;
 
 pub mod second;
 
+// SortOrderを列挙型として定義する
+pub enum SortOrder {
+    Ascending,  // 昇順
+    Descending, // 降順
+}
--- bitonic-sorter/src/second.rs
+++ bitonic-sorter/src/second.rs
@@ -1,18 +1,33 @@
-﻿// pub はこのsort関数がほかのモジュールからアクセスできることを示す
+﻿use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String>{
+    if x.len().is_power_of_two(){
+        match *order{
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    }else{
+        Err(format!("The length of x is no a power of two. (x.len(): {}", x.len()))
+    }
+}
+
+
+// pub はこのsort関数がほかのモジュールからアクセスできることを示す
 // 引数 x の型　&mut[u32]について
 // & は値をポインタ経由で借用することを示す
 // [u32]型は u32 のスライス ( 現時点でスライスは1次元の配列と考えてよい )
-pub fn sort<T>(x: &mut[T], up: bool){
+fn do_sort<T: Ord>(x: &mut[T], up: bool){
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up);
     }
 }
 
 
-fn sub_sort<T>(x: &mut[T], up: bool){
+fn sub_sort<T: Ord>(x: &mut[T], up: bool){
     if x.len() > 1 {
         compare_and_swap(x, up);
         let mid_point = x.len() / 2;
@@ -22,7 +37,7 @@ fn sub_sort<T>(x: &mut[T], up: bool){
 }
 
 
-fn compare_and_swap<T>(x: &mut[T], up: bool){
+fn compare_and_swap<T: Ord>(x: &mut[T], up: bool){
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
@@ -38,6 +53,15 @@ mod tests{
 
     // 親モジュール (first) のsort関数を使用する
     use super::sort;
+    use crate::SortOrder::*;
+
+
+    #[test]
+    fn sort_to_fail(){
+        let mut x = vec![10, 30, 11]; // x.len()が2のべき乗になっていない
+        assert!(sort(&mut x, &Ascending).is_err()); // 返り値はErr
+    }
+
 
     // #[test]のついた関数は cargo test としたときに実行される
     #[test]
@@ -48,7 +72,7 @@ mod tests{
 
         // x のスライスを作成し、sort関数を呼び出す
         // &mut x は &mut x[..] と書いても良い
-        sort(&mut x, true);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
 
         // x の要素が昇順にソートされていることを確認する
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
@@ -58,7 +82,7 @@ mod tests{
     #[test]
     fn sort_u32_descending(){
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, false);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
 
         // x の要素が降順にソートされていることを確認する
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
@@ -69,7 +93,7 @@ mod tests{
     fn sort_str_ascending(){
         // 文字列のベクタを作成し、ソート
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, true);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
         assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
     }
     
@@ -77,7 +101,7 @@ mod tests{
     #[test]
     fn sort_str_descending(){
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, false);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
         assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
     }
 }
