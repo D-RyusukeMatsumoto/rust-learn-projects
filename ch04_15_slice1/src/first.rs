c0cd81cd8da4cd800fe0a900e90211ee875232e3
D-RyusukeMatsumoto <matsumoto@dedication.co.jp>
2020-06-01 00:46:33 +0900

    サンプルごとにファイルを分けて扱うことでよりサンプルをテストしやすくした

---
+++ ch04_15_slice1/src/first.rs
@@ -0,0 +1,5 @@
+
+
+pub fn sample() {
+    print!("HelloSample1");
+}
--- ch04_15_slice1/src/main.rs
+++ ch04_15_slice1/src/main.rs
@@ -1,3 +1,8 @@
+
+// 各サンプルの名前をこのファイルで利用する名前に変更
+use ch04_15_slice1::first::sample as first_sample;
+
 fn main() {
-    println!("Hello, world!");
+    first_sample();
 }
+
